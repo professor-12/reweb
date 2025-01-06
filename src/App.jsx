3a378dd9596e393f92822c6eb52fead8356eff25
emmanuel <badejoemmanuel805@gmail.com>
2025-01-06 13:54:51 -0800

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-[hsl(var(--background))] w-full h-[100vh]'></div>
+
+  )
+}
+
+export default App
